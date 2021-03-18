32c0af2fca1dd086236bb9a9411ffad28b37e907
Mitsutoshi Aoe <me@maoe.name>
2021-03-18 09:59:08 +0900

    Add a test to demonstrate numerical errors
    This test passes on Ubuntu Bionic but fails on Debian Bullseye.

--- src/piecewise.rs
+++ src/piecewise.rs
@@ -1512,4 +1512,22 @@ mod tests {
             1e-10
         );
     }
+
+    #[test]
+    fn evaluate_segment_log_poly4() {
+        let segment = Segment {
+            end: 1.1276353206339418,
+            poly: IntOfLogPoly4 {
+                k: 0.0052622775209652944,
+                coeffs: [
+                    -0.8000546687895224,
+                    2.7643402381079487,
+                    26.287021612040366,
+                    109.18921102766446,
+                ],
+                u: 22684.248437352602,
+            },
+        };
+        assert_eq!(segment.evaluate(segment.end), 0.12764305320171115_f64);
+    }
 }
