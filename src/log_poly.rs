6b92342b7fa8fa36dd13669875a34a518816b190
Mitsutoshi Aoe <me@maoe.name>
2021-03-19 13:11:39 +0900

    log_poly: Add a test to demonstrate difference between different libc implementations

--- src/log_poly.rs
+++ src/log_poly.rs
@@ -786,4 +786,19 @@ mod tests {
         let knot = Knot { x: 1.0, y: 2.0 };
         assert_eq!(poly.integral(knot), result);
     }
+
+    #[test]
+    fn evaluate_int_of_log_poly4() {
+        let poly = IntOfLogPoly4 {
+            k: 0.0052622775209652944,
+            coeffs: [
+                -0.8000546687895224,
+                2.7643402381079487,
+                26.287021612040366,
+                109.18921102766446,
+            ],
+            u: 22684.248437352602,
+        };
+        assert_eq!(poly.evaluate(1.1276353206339418), 0.12764305320171115);
+    }
 }
